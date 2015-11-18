f7130b317e64433c2a39dae4f0f12585aff235ca
Luiz Gustavo Gavinho <lgavinho@gmail.com>
2015-11-18 01:05:21 -0200

    create a facade to add, remove, update and list user in google apps

--- AccountFacade.php
+++ AccountFacade.php
@@ -1,17 +1,139 @@
 <?php
+session_start();
 
 require 'vendor/autoload.php';
+require 'UserModel.php';
 
 class AccountFacade
 {
+    private $GoogleServerAPIKey = '';
+    private $client_id = '';
+    private $service_account_name = ''; //Email Address
+    private $key_file_location = ''; //key.p12
+    private $user_to_impersonate = '';
+    private $domain = '';
+    private $developerKey = '';
+    private $scopes = array(
+        'https://www.googleapis.com/auth/admin.directory.user',
+        'https://www.googleapis.com/auth/admin.directory.group'
+    );
+
+    private $client = Null;
+    private $service = Null;
+
+
+    private function parseCredentials()
+    {
+        $string = file_get_contents("credentials.json");
+        $credentials = json_decode($string, true);
+        $this->GoogleServerAPIKey = $credentials['GoogleServerAPIKey'];
+        $this->client_id = $credentials['client_id'];
+        $this->service_account_name = $credentials['service_account_name'];
+        $this->key_file_location = $credentials['key_file_location'];
+        $this->user_to_impersonate = $credentials['user_to_impersonate'];
+        $this->domain = $credentials['domain'];
+        $this->developerKey = $credentials['developerKey'];
+    }
 
     public function __construct()
     {
-
+        $this->parseCredentials();
+        $this->client = new Google_Client();
+        $this->client->setApplicationName("Smartlab");
+        $this->client->setDeveloperKey($this->developerKey);
+        if (isset($_SESSION['service_token'])) {
+            $this->client->setAccessToken($_SESSION['service_token']);
+        }
+        $key = file_get_contents($this->key_file_location);
+        $cred = new Google_Auth_AssertionCredentials(
+            $this->service_account_name,
+            $this->scopes,
+            $key,
+            'notasecret', // Default P12 password
+            'http://oauth.net/grant_type/jwt/1.0/bearer', // Default grant type
+            $this->user_to_impersonate
+        );
+        $this->client->setAssertionCredentials($cred);
+        if ($this->client->getAuth()->isAccessTokenExpired()) {
+            $this->client->getAuth()->refreshTokenWithAssertion($cred);
+        }
+        $_SESSION['service_token'] = $this->client->getAccessToken();
+        $this->service = new Google_Service_Directory($this->client);
     }
 
-    public function ping()
+    /**
+     * @return Google_Service_Directory_Users
+     *
+     * Iterate the return $userList->getUsers() as $user
+     * echo $user->name->fullName for example.
+     *
+     */
+    public function getUsers()
     {
+        $userList = $this->service->users->listUsers(array(
+            'domain'        => $this->domain,
+            'maxResults'    => 100,
+            'orderBy'       => 'email',
+            'sortOrder'     => 'ASCENDING'
+        ));
+        return $userList;
+    }
+
+    /**
+     * @param UserModel $user
+     * @return bool
+     */
+    public function addUser(UserModel $user)
+    {
+        try {
+            $new_user = $this->service->users->insert($user->getGoogleUser());
+        }
+        catch (Google_Service_Exception $e) {
+            return False;
+        }
+        return True;
+    }
+
+    /**
+     * @param string $email
+     * @return bool
+     */
+    public function removeUserByEmail($email)
+    {
+        try {
+            $result = $this->service->users->delete($email);
+            if ($result != Null) {
+                return False;
+            }
+        }
+        catch (Google_Service_Exception $e) {
+            return False;
+        }
+        return True;
+    }
+
+    /**
+     * @param string $email
+     * @return Google_Service_Directory_User
+     */
+    public function getUserByEmail($email)
+    {
+        $user = $this->service->users->get($email);
+        return $user;
+    }
+
+    /**
+     * @param UserModel $user
+     * @return bool
+     */
+    public function updateUser(UserModel $user)
+    {
+        try {
+            $updated = $this->service->users->patch($user->getPrimaryEmail(), $user->getGoogleUser());
+        }
+        catch (Google_Service_Exception $e) {
+            return False;
+        }
         return True;
     }
 }
--- test/AccountFacadeTest.php
+++ test/AccountFacadeTest.php
@@ -4,14 +4,60 @@ require 'AccountFacade.php';
 
 class AccountFacadeTest extends PHPUnit_Framework_TestCase
 {
+    private $domain = 'gedu.demo.smartlab.me';
+    private $user1 = Null;
+    private $account = Null;
+
     protected function setUp()
     {
+        $email = 'test1@' . $this->domain;
+        $user1 = new UserModel();
+        $user1->setGivenName('Tester');
+        $user1->setPrimaryEmail($email);
+        $user1->setFamilyName('Testerton');
+        $user1->setPassword('123');
+        $this->user1 = $user1;
 
+        $this->account = new AccountFacade();
     }
 
-    public function testPing()
+    public function testAddUserSuccess()
     {
-        $object = new AccountFacade();
-        $this->assertTrue($object->ping());
+        $newUser = $this->account->addUser($this->user1);
+        $this->assertTrue($newUser);
+
+        $googleUser = $this->account->getUserByEmail($this->user1->getPrimaryEmail());
+        $this->assertInstanceOf('Google_Service_Directory_User', $googleUser);
     }
+
+    public function testRemoveInvalidUser()
+    {
+        $result = $this->account->removeUserByEmail('invaliduser@' . $this->domain);
+        $this->assertFalse($result);
+    }
+
+    public function testUpdateUserSuccess()
+    {
+        $user = clone $this->user1;
+        $user->setPassword('matrix');
+        $result = $this->account->updateUser($user);
+        $this->assertTrue($result);
+    }
+
+    public function testListOfUsersSuccess()
+    {
+        $list = $this->account->getUsers();
+        $this->assertInstanceOf('Google_Service_Directory_Users',$list);
+        echo PHP_EOL;
+        foreach ($list->getUsers() as $user) {
+            echo $user->name->fullName . PHP_EOL;
+        }
+    }
+
+    public function testRemoveUserSuccess()
+    {
+        $result = $this->account->removeUserByEmail($this->user1->getPrimaryEmail());
+        $this->assertTrue($result);
+    }
+
 }
